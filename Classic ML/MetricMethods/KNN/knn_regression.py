ce235d1fb54006da0d6ad1d8102687298371aba8
alexborodach30 <sagopien04@gmail.com>
2026-03-08 12:57:34 +0300

    KNN Regression expanded to other metrics.

--- Classic ML/MetricMethods/KNN/knn_regression.py
+++ Classic ML/MetricMethods/KNN/knn_regression.py
@@ -10,14 +10,17 @@ class KNNRegressor:
     methods: constructor, fit, predict
     """
 
-    def __init__(self, k: int = 2):
+    def __init__(self, k: int = 2, metric: str = "euclidean", p: int = 2):
         """
         Constructor
         :param k: number of neighbours
-        :return: None
+        :param metric: distance metric (euclidean, manhattan, minkowski, cosine)
+        :param p: parameter for minkowski distance
         """
 
         self.k = k
+        self.metric = metric
+        self.p = p
 
     def fit(self, X: np.ndarray, y: np.ndarray) -> None:
         """
@@ -29,6 +32,26 @@ class KNNRegressor:
 
         self.X_train = X
         self.y_train = y
+
+    def _distance(self, x: np.ndarray) -> np.ndarray:
+        if self.metric == "euclidean":
+            return np.sqrt(np.sum((self.X_train - x) ** 2, axis=1))
+
+        elif self.metric == "manhattan":
+            return np.sum(np.abs(self.X_train - x), axis=1)
+
+        elif self.metric == "minkowski":
+            return np.sum(np.abs(self.X_train - x) ** self.p, axis=1) ** (1 / self.p)
+
+        elif self.metric == "cosine":
+            dot = np.dot(self.X_train, x)
+            norm_train = np.linalg.norm(self.X_train, axis=1)
+            norm_x = np.linalg.norm(x)
+            cosine_similarity_custom = dot / (norm_train * norm_x + 1e-10)
+            return 1 - cosine_similarity_custom
+
+        else:
+            raise ValueError("Unsuppoerted metric")
 
     def predict(self, X_test: np.ndarray) -> np.ndarray:
         """
@@ -43,7 +66,7 @@ class KNNRegressor:
 
         predictions = []
         for x in X_test:
-            distances = np.sqrt(np.sum((self.X_train - x) ** 2, 1))
+            distances = self._distance(x)
             k_indices = np.argsort(distances)[:self.k]
             k_nearest_targets = self.y_train[k_indices]
             prediction = np.mean(k_nearest_targets)
@@ -57,12 +80,37 @@ my_knn_reg = KNNRegressor(k=5)
 my_knn_reg.fit(Xr_train, yr_train)
 yr_pred_my = my_knn_reg.predict(Xr_test)
 
+my_knn_reg_manhattan = KNNRegressor(k=5, metric="manhattan")
+my_knn_reg_manhattan.fit(Xr_train, yr_train)
+yr_pred_my_manhattan = my_knn_reg.predict(Xr_test)
+
+my_knn_reg_minkowski = KNNRegressor(k=5, metric="minkowski")
+my_knn_reg_minkowski.fit(Xr_train, yr_train)
+yr_pred_my_minkowski = my_knn_reg.predict(Xr_test)
+
+my_knn_reg_cosine = KNNRegressor(k=5, metric="cosine")
+my_knn_reg_cosine.fit(Xr_train, yr_train)
+yr_pred_my_cosine = my_knn_reg.predict(Xr_test)
+
 sk_knn_reg = KNeighborsRegressor(n_neighbors=5)
 sk_knn_reg.fit(Xr_train, yr_train)
 yr_pred_sk = sk_knn_reg.predict(Xr_test)
 
+print("Custom KNNRegressor Euclidean")
 print("Custom KNNRegressor MSE:", mean_squared_error(yr_test, yr_pred_my))
 print("Custom KNNRegressor R2:", r2_score(yr_test, yr_pred_my))
 print("\n")
-print("Custom KNNRegressor MSE:", mean_squared_error(yr_test, yr_pred_sk))
+print("Custom KNNRegressor Manhattan")
+print("Custom KNNRegressor MSE:", mean_squared_error(yr_test, yr_pred_my_manhattan))
+print("Custom KNNRegressor R2:", r2_score(yr_test, yr_pred_my_manhattan))
+print("\n")
+print("Custom KNNRegressor Minkowski")
+print("Custom KNNRegressor MSE:", mean_squared_error(yr_test, yr_pred_my_minkowski))
+print("Custom KNNRegressor R2:", r2_score(yr_test, yr_pred_my_minkowski))
+print("\n")
+print("Custom KNNRegressor Cosine")
+print("Custom KNNRegressor MSE:", mean_squared_error(yr_test, yr_pred_my_cosine))
+print("Custom KNNRegressor R2:", r2_score(yr_test, yr_pred_my_cosine))
+print("\n")
+print("Sklearn KNNRegressor MSE:", mean_squared_error(yr_test, yr_pred_sk))
 print("Sklearn KNNRegressor R2:", r2_score(yr_test, yr_pred_sk))
