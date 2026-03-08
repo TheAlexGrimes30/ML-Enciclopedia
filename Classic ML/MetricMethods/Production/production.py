2946bb7149c77e68bc9d0e80ecf3cca2301d0eea
alexborodach30 <sagopien04@gmail.com>
2026-03-08 20:45:12 +0300

    KNN Prodction.

--- Classic ML/MetricMethods/Production/production.py
+++ Classic ML/MetricMethods/Production/production.py
@@ -2,9 +2,10 @@ from collections import Counter
 
 import faiss
 from sklearn.datasets import make_classification
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.model_selection import train_test_split
 from annoy import AnnoyIndex
+
 X, y = make_classification(
     n_samples=5000,
     n_features=20,
@@ -40,7 +41,15 @@ for x in X_test:
     predictions_annoy.append(prediction)
 
 acc_annoy = accuracy_score(y_test, predictions_annoy)
+precision_annoy = precision_score(y_test, predictions_annoy)
+recall_annoy = recall_score(y_test, predictions_annoy)
+f1_annoy = f1_score(y_test, predictions_annoy)
+
 print("Annoy Accuracy:", acc_annoy)
+print("Annoy Precision:", precision_annoy)
+print("Annoy Recall:", recall_annoy)
+print("Annoy F1:", f1_annoy)
+print()
 
 faiss_index = faiss.IndexFlatL2(dim)
 faiss_index.add(X_train)
@@ -51,5 +60,13 @@ for neighbors in indices:
     neighbor_labels = y_train[neighbors]
     prediction = Counter(neighbor_labels).most_common(1)[0][0]
     predictions_faiss.append(prediction)
+
 acc_faiss = accuracy_score(y_test, predictions_faiss)
+precision_faiss = precision_score(y_test, predictions_faiss)
+recall_faiss = recall_score(y_test, predictions_faiss)
+f1_faiss = f1_score(y_test, predictions_faiss)
+
 print("Faiss Accuracy:", acc_faiss)
+print("Faiss Precision:", precision_faiss)
+print("Faiss Recall:", recall_faiss)
+print("Faiss F1:", f1_faiss)
